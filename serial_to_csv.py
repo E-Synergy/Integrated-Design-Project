87d2c7bdcac3074134c6520d954e8d33f8034ac3
Li Yuen Tay <liyuentay1603@gmail.com>
2026-07-15 13:38:20 +0800

    MPU: Python script successfully outputs relevant data in Excel. (In root folder screenshot)

--- serial_to_csv.py
+++ serial_to_csv.py
@@ -1,31 +1,49 @@
 import serial
 import csv
+import time 
 
 arduino_port = "COM8"
 baud_rate = 115200
 filename = "data.csv"
+headers = ["Date", "Time", "Accel_X", "Accel_Y", "Accel_Z", "Acc_Magnitude", "Jerk_Magnitude"]
 
 def process_sensor_data(ser, writer, file):
     if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').strip()
-        if line.count(',') == 5:
+        if line.count(',') == (len(headers) - 3):
             data_list = line.split(',')
+
+            current_date = time.strftime('%Y-%m-%d')
+            current_time = time.strftime('%H:%M:%S')
+
+            first_index = 0
+            data_list.insert(first_index, current_time)
+            data_list.insert(first_index, current_date)
+
             writer.writerow(data_list)
             file.flush() # Force save to disk
-            print(f"Logged Accel (m/s^2) & Gyro (rad/s): {data_list}")
+            print(f"{data_list}")
+            return True
+            
         else:
             print(f"Ignored non-data text: {line}")
+            return False
+    return False
 
 def write(ser, file):
     writer = csv.writer(file)
-    headers = ["Accel_X", "Accel_Y", "Accel_Z", "Gyro_X", "Gyro_Y", "Gyro_Z"]
     writer.writerow(headers)
 
     print(f'Start logging')
 
     try:
-        while True:
-            process_sensor_data(ser, writer, file)
+        count = 0
+        max_data_points = 1000
+        while True and count < max_data_points:
+            is_successful = process_sensor_data(ser, writer, file)
+            if(is_successful):
+                count += 1
+                print(f'Saved {count} of  {max_data_points}')
 
     except KeyboardInterrupt:
         print("\nLogging stopped by user.")
@@ -42,7 +60,7 @@ except Exception as e:
     print(f'Error: { e }')
     exit()
 
-with open(filename, "a", newline = '') as file:
+with open(filename, "r+", newline = '') as file:
     #with statement isolates memory and by having an explicit statement to close the block further on, memory leak does not occur.
     #newline parameter just to address CRLF preferences; important for csv files.
     #"a" points to append and creates new file if not exists, and preserves existing data if exists
